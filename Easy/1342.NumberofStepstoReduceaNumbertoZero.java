f6f92eb28f9563a9b771f0679853a37f5a1ba95b
Harjit <44533624+Slaeh@users.noreply.github.com>
2020-08-18 20:15:52 -0400

    new problem

---
+++ Easy/1342.NumberofStepstoReduceaNumbertoZero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSteps (int num) {
+        int steps = 0;
+        while(num != 0){
+            if(num % 2 == 0){
+                num /= 2;
+                steps++;
+            }else{
+                if(num % 2 == 1){
+                    num -= 1;
+                    steps++;
+                }
+            }
+        }
+        return steps;
+    }
+}
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
