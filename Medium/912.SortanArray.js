dba8ec2e74300f25c4ed6642e38d5874650aab74
Harjit <harjitliyal@gmail.com>
2021-08-15 20:00:34 -0400

    Another one

---
+++ Medium/912.SortanArray.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+  return nums.sort((a, b) => a - b);
+};
+//https://leetcode.com/problems/sort-an-array/
